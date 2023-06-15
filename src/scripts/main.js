c97100ad1669adf590dfb52034e9557675e1f70e
MSixels <msixel22@gmail.com>
2023-06-15 16:50:18 -0300

    criando a contagem regressiva

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,28 @@
-console.log("olá mundo");
+    AOS.init();
+
+    const dataDoEvento = new Date("Sep 16, 2023 12:00:00");
+    const timeStampDoEvento = dataDoEvento.getTime();
+
+    const contaAsHoras = setInterval(function (){
+        const agora = new Date();
+        const timeStampAtual = agora.getTime();
+
+        const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
+        const diasEmMS = 1000 * 60 * 60 * 24;
+        const horaEmMS = 1000 * 60 * 60;
+        const minutoEmMs = 1000* 60;
+        
+
+        const diasAteOEvento = Math.floor(distanciaAteOEvento / diasEmMS);
+        const horasAteOEvento = Math.floor((distanciaAteOEvento % diasEmMS) / horaEmMS);
+        const minutosAteOEvento = Math.floor((distanciaAteOEvento % horaEmMS) / minutoEmMs);
+        const segundosAteOEvento = Math.floor((distanciaAteOEvento % minutoEmMs) / 1000);
+        
+
+        document.getElementById('contador').innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`;
+
+        if(distanciaAteOEvento < 0 ) {
+            clearInterval(contaAsHoras);
+            document.getElementById('contador').innerHTML = `Evento Expirado`
+        }
+    }, 1000);
